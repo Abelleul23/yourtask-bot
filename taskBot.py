781a0f3411ba6cb133005e060939bf901af19a3b
Abel Leul <Abelteamr@gmail.com>
2024-03-12 23:01:34 +0300

    Add task reminder feature to the bot

--- taskBot.py
+++ taskBot.py
@@ -1,5 +1,5 @@
 import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, Bot
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -7,8 +7,14 @@ from telegram.ext import (
     ConversationHandler,
     MessageHandler,
     filters,
+    Updater,
 )
 from pymongo import MongoClient
+from datetime import datetime, time, timezone, timedelta
+import asyncio
+import queue
+import datetime as dt
+
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['task_management_bot']
@@ -23,7 +29,7 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-TASK_TITLE, TASK_DESCRIPTION = range(2)
+TASK_TITLE, TASK_DESCRIPTION, SET_DUE_DATE = range(3)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -58,27 +64,19 @@ async def skip_title(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 async def task_description(update, context):
     context.user_data['description'] = update.message.text
 
-    task = {
-        'title': context.user_data['title'],
-        'description': context.user_data['description']
-    }
-    tasks_collection.insert_one(task)
-
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Okay, Your task is saved")
-    print('good for you!')
-    return ConversationHandler.END
+        chat_id=update.effective_chat.id, text="Okay, set due date")
+    return SET_DUE_DATE
 
 
 async def skip_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the photo and asks for a location."""
     user = update.message.from_user
     logger.info("User %s did not send a description.", user.first_name)
     await update.message.reply_text(
         "Enter task description, or send /skip."
     )
 
-    return ConversationHandler.END
+    return SET_DUE_DATE
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -92,17 +90,83 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return ConversationHandler.END
 
 
+async def save_task(context):
+    due_date = datetime.combine(context.user_data['due_date'], time())
+    task = {
+        'title': context.user_data['title'],
+        'description': context.user_data['description'],
+        'due_date': due_date,
+        'chat_id': context._chat_id,
+    }
+    tasks_collection.insert_one(task)
+
+
+async def set_due_date(update, context):
+    if update.message.text:
+        due_date = datetime.strptime(
+            update.message.text, '%Y-%m-%d').date()
+        context.user_data['due_date'] = due_date
+
+        # Save the task to the database
+        await save_task(context)
+
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Task due date set to {due_date}."
+        )
+
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Invalid due date format. Please enter the due date for the task (YYYY-MM-DD):"
+        )
+
+    return ConversationHandler.END
+
+
+async def remind_tasks(context):
+    while True:
+        # Get the current date and time
+        current_date = dt.date.today()
+
+        # Retrieve tasks from the database
+        tasks = tasks_collection.find()
+
+        # Iterate over tasks and send reminders if due date is reached
+        for task in tasks:
+            due_date = task['due_date'].date()
+            if due_date == current_date:
+                chat_id = task['chat_id']
+                task_title = task['title']
+                message = f"Reminder: Task '{task_title}' is due today!"
+                await context.bot.send_message(chat_id=chat_id, text=message)
+
+        # Wait for some time before checking tasks again
+        await asyncio.sleep(600)  # Check every minute
+
+
 def main() -> None:
     """Run the bot."""
+    bot_token = "6427831805:AAEDbX7HuYZ1nXI_SC9-cmgkH7NQLQeY0Rk"
+    bot = Bot(token=bot_token)
+    update_queue = queue.Queue()
+
+    updater = Updater(bot=bot, update_queue=update_queue)
+
+    context = updater
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(remind_tasks(context))
 
     application = Application.builder().token(
-        "TOKEN").build()
+        "6427831805:AAEDbX7HuYZ1nXI_SC9-cmgkH7NQLQeY0Rk").build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
             TASK_TITLE: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, task_title)],
             TASK_DESCRIPTION: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, task_description)],
+            SET_DUE_DATE: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, set_due_date)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
